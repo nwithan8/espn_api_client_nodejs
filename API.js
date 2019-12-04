56d8f365ef8406d568e0b0c6d90acea15fef4b79
Nate Harris <n8gr8gbln@gmail.com>
2019-12-04 14:46:52 -0500

    Boilerplate

---
+++ API.js
@@ -0,0 +1,47 @@
+const base = 'http://site.api.espn.com/apis/site/v2/sports/'
+
+function get_sport(league) {
+    // if league = college-football or nfl, sport = football
+    // if league = mens-college-basketball, womens-college-basketball, nba or wnba, sport = basketball
+    // if league = college-baseball or mlb, sport = baseball
+    // if league = nhl, sport = hockey
+    // else: ASSUME SOCCER, sport = soccer
+}
+
+function get_conference_id(conference) {
+    //get conference id from a conference name/abbreviation
+}
+
+function get_news(league) {
+    
+}
+
+function get_scoreboard(league) {
+    
+}
+
+function get_team(league, team) {
+    //'team' can be teamID or abbreviation
+    
+}
+
+function get_team_schedule(league, teamID) {
+    
+}
+
+function get_game(league, gameID) {
+    
+}
+
+function get_game_probability(league, gameID) {
+    
+}
+
+function get_game_odds(league, gameID) {
+    
+}
+
+function get_rankings(league) {
+    //'leaderboard' for things like golf, 'rankings' for team sports
+}
+
