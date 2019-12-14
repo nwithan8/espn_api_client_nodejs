3fa67855ddd03ba7007cb6570e47ca80c39be7a4
Nate Harris <n8gr8gbln@gmail.com>
2019-12-14 16:24:58 -0500

    Test.js updated

--- test.js
+++ test.js
@@ -2,13 +2,35 @@ const ESPN = require('.')
 
 const main = async () => {
   const espn = new ESPN()
-  const league = espn.League('mens-college-basketball')
-  const team = await league.Team('ucla')
+  const league = espn.League('college-football')
+  const currentGames = await league.getCurrentGames()
+  // await league.getTeams()
+  // const SEC = await league.Conference('8')
+  // console.log(await SEC.getTeams())
 
-  // const league = espn.League('nfl')
-  // console.log(league)
-  // const team = league.Team('patriots')
-  // console.log(team)
+
+  // console.log(currentGames)
+  const game = currentGames[0]
+  // console.log(game)
+  await game.makeGame()
+  console.log(game)
+  /* console.log(await game.getGameOdds())
+  console.log(game.spread)
+  console.log(team.teamID)
+  console.log(await team.getTeamLogoURL()) */
+  // console.log(await team.getJson())
+  // console.log(await team.getTeamRoster())
+  // await team.makeTeam()
+  // console.log(team.schedule)
+  // const schedule = await team.getTeamSchedule()
+  // const game1 = schedule[0]
+  // console.log(await game1.getJson())
+  /* game = await league.Game('401128117')
+  console.log(game)
+  console.log(game.baseUrl)
+  console.log(await game.getJson())
+  const player = await league.Player('101')
+  await player.updatePlayerInfo()
+  console.log(player) */
 }
-
 main()
